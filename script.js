483fbe1b9395173c1c29c14e4a3775dae78616d0
alexfortin <fortin.alex14@gmail.com>
2022-09-20 22:53:58 -0500

    can now call the api and acces the data

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
+var weatherKey = "e365a56e35842cef898de05f21dfc4b8"
 var cityForm = $("#search-city-form")
 var cityInput = $("#city-input")
 
-var getInfo = function(event) {
+var getInfo = function (event) {
     event.preventDefault()
     var cityName = $(cityInput)
-    .val()
-    .trim()
+        .val()
+        .trim()
     console.log(cityName)
 
-    if(cityName) {
-        console.log("Succes" + cityName)
+    if (cityName) {
+        getRepoData(cityName)
     }
     else {
-        alert("Fail")
+        alert("Please enter a city name")
     }
     
+}
+
+var getRepoData = function (cityName) {
+
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + weatherKey
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data)
+            })
+        }
+        else {
+            alert("Not a valid city, please try again")
+            return
+        }
+    })
+    .catch(function(error){
+        console.log(error)
+    })
 }
 
 $(cityForm).on("submit", getInfo)
