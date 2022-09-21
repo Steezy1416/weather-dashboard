06ac04d3f9547077a5e677a3573aee60a0cf346e
alexfortin <fortin.alex14@gmail.com>
2022-09-21 14:33:25 -0500

    added feature to display the current weather data for the city that the user types in

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ var getInfo = function (event) {
 
     if (cityName) {
         getRepoData(cityName)
+        $(cityInput).val("")
     }
     else {
         alert("Please enter a city name")
     }
-    
 }
 
 var getRepoData = function (cityName) {
@@ -25,6 +25,7 @@ var getRepoData = function (cityName) {
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
+                displayCurrentData(data)
                 console.log(data)
             })
         }
@@ -38,4 +39,30 @@ var getRepoData = function (cityName) {
     })
 }
 
+var displayCurrentData = function(data) {
+    var currentCity = $("#city-name")
+    var currentDate = $("#current-date")
+    var currentTemp = $("#current-temp")
+    var currentWind = $("#current-wind")
+    var currentHumidity = $("#current-humidity")
+    var weatherIcon = data.list[0].weather[0].icon
+
+    var imageContainer = document.querySelector("#image-container")
+    var icon = document.createElement("img")
+    icon.src = "http://openweathermap.org/img/wn/" + weatherIcon + ".png"
+
+    var city = data.city.name
+    var date = moment().format("l")
+    var temp = "Temp: " + data.list[0].main.temp + "°F"
+    var wind = "Wind: " + data.list[0].wind.speed + " Mph"
+    var humidity = "Humidity: " + data.list[0].main.humidity + "%"
+
+    $(currentCity).empty().append(city)
+    $(currentDate).empty().append(date)
+    $(imageContainer).empty().append(icon)
+    $(currentTemp).empty().append(temp)
+    $(currentWind).empty().append(wind)
+    $(currentHumidity).empty().append(humidity)
+    
+}
 $(cityForm).on("submit", getInfo)
