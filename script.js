9bd3df0f9b34531764470d0c89bb0a8066a8198b
alexfortin <fortin.alex14@gmail.com>
2022-09-22 22:25:56 -0500

    added readme and comments to the code

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ var clearStorage = $("#clear-btn")
 var formContainer = $(".placeholder-form-container")
 var mainInformation =$(".main-information")
 
-//id="form-container"
-
 var getInfo = function (event) {
     event.preventDefault()
+    //saves users input to a city
     var cityName = $(cityInput)
         .val()
         .trim()
     console.log(cityName)
 
+    //if there is input it gets the data from api else it tells you enter a city
     if (cityName) {
         getRepoData(cityName)
         $(cityInput).val("")
@@ -27,9 +27,11 @@ var getInfo = function (event) {
 
 var getRepoData = function (cityName) {
 
+    //uses users input and it put in the api call to grab its data
     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + weatherKey
 
     fetch(apiUrl).then(function (response) {
+        //if the city is a valid or real city it will display the city's weather data
         if (response.ok) {
             response.json().then(function (data) {
                 displayCurrentData(data)
@@ -50,7 +52,6 @@ var getRepoData = function (cityName) {
 var displayCurrentData = function (data) {
     formContainer.removeClass("placeholder-form-container")
     mainInformation.removeClass("hide")
-    
 
     var currentCity = $("#city-name")
     var currentDate = $("#current-date")
@@ -63,12 +64,14 @@ var displayCurrentData = function (data) {
     var icon = document.createElement("img")
     icon.src = "http://openweathermap.org/img/wn/" + weatherIcon + ".png"
 
+    //saves the correct weather information into the variables
     var city = data.city.name
     var date = moment().format("l")
     var temp = "Temp: " + data.list[0].main.temp + "°F"
     var wind = "Wind: " + data.list[0].wind.speed + " Mph"
     var humidity = "Humidity: " + data.list[0].main.humidity + "%"
 
+    //appends the data to the city info container
     $(currentCity).empty().append(city)
     $(currentDate).empty().append(date)
     $(imageContainer).empty().append(icon)
@@ -83,8 +86,10 @@ var displayCurrentData = function (data) {
 
 var displayForecast = function (data) {
     var dayIncrement = 1
+    //picked a specific time for the forecast so it loops trough the arrays at specific increments to get a specific time for each day
     for (var i = 5; i < data.list.length; i += 8) {
 
+        //setting up the information to be based on the increment
         var date = moment(moment()).add(dayIncrement, 'days').format('l')
         var weathericon = data.list[i].weather[0].icon
         var imageFor = document.querySelector("#image-container-for-" + i)
@@ -101,6 +106,7 @@ var displayForecast = function (data) {
         var windFor = $("#wind-for-" + i)
         var humidityFor = $("#humidity-for-" + i)
 
+        //appends all the elements
         $(dateFor).empty().append(date)
         $(imageFor).empty().append(imgIcon)
         $(tempFor).empty().append(temp)
@@ -111,6 +117,7 @@ var displayForecast = function (data) {
 }
 
 var cityHistory = function(city) {
+    //creates new elements to store the city title or city "history"
     var historyRow = $(".history-row")
     var historyBox = document.createElement("div")
     historyBox.classList = "col-3 history-box"
@@ -121,22 +128,24 @@ var cityHistory = function(city) {
     historyRow.append(historyBox)
     numberOfHistoryBoxes ++
 
+    //saves the city name and number of cities saved into local storage
     localStorage.setItem(numberOfHistoryBoxes, city)
-
-
     
 }
 
 $(cityForm).on("submit", getInfo)
+
+//when you click on the history buttons it grabs the name of the city and runs it through the get repo data function to run the whole program again
 $(historyContainer).on("click",".city-history", function(event) {
     var historyCity = event.target.innerText
     getRepoData(historyCity)
 })
+//if storage gets to crowded user can clear the local storage
 $(clearStorage).on("click", function(){
     localStorage.clear()
 })
 
-
+//displays the history of cities based on the ammount of cities saved
 for (var i = 0; i < localStorage.length; i++) {
     var storedCity = (localStorage.getItem(i))
     cityHistory(storedCity)
